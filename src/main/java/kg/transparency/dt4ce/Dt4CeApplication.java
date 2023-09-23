929ecc2f1e2643c46207c6856dde878dcb22d550
Diyorka <diyorp2@mail.ru>
2023-09-23 13:18:14 +0600

    Initial commit

---
+++ src/main/java/kg/transparency/dt4ce/Dt4CeApplication.java
@@ -0,0 +1,13 @@
+package kg.transparency.dt4ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dt4CeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dt4CeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/transparency/dt4ce/Dt4CeApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.transparency.dt4ce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dt4CeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
